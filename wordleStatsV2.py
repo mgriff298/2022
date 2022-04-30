98e780b71f9825d2613e194b61a3941e81df9114
mgriff298 <66095974+mgriff298@users.noreply.github.com>
2022-04-30 13:32:59 -0700

    Add files via upload

--- wordleStatsV2.py
+++ wordleStatsV2.py
@@ -1,16 +1,23 @@
+from cProfile import run
 from curses.ascii import BS
+from glob import glob
 from pickle import FALSE, TRUE
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
+# ingest words and create initial dataframe 
 def readData():
     global numOfGuesses
     numOfGuesses = 0
+
     wordListInit = pd.read_excel('Wordle_words.xlsx', sheet_name='FullList')
     wordListInit['active'] = 1
     wordListInit['notGuessed'] = 1
+    wordListInit['repeatLets'] = 1
     return wordListInit
 
+# create dataframe of letters
 def containLets(wordList):
     lets = []
     letsList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
@@ -21,6 +28,7 @@ def containLets(wordList):
 
     return dfLets
 
+# create full dataframe used moving forward, includes analysis of which letters will yield the most information when guessed
 def createFullDf(wordList2):
     wordList = wordList2[wordList2['active']==1]
 
@@ -81,24 +89,28 @@ def createFullDf(wordList2):
     
     return wordList
 
+# calculates the information score after each guess
 def calcScore(wordListScore):
     global numOfGuesses
-    if numOfGuesses == 0:
-        wordListScore.loc[:,('active')] = np.where(wordListScore['Words'].apply(lambda x: len(set(x)))!=5, 0, wordListScore['active'])
-        wordListScore.loc[:,('score')] = (wordListScore['firstLetVal'] + wordListScore['secondLetVal'] + wordListScore['thirdLetVal'] + wordListScore['fourthLetVal'] + wordListScore['fifthLetVal'])*wordListScore['active']
+    if numOfGuesses == 0 or numOfGuesses == 1:
+        wordListScore.loc[:,('repeatLets')] = np.where(wordListScore['Words'].apply(lambda x: len(set(x)))!=5, 0, wordListScore['repeatLets'])
+        wordListScore.loc[:,('score')] = (wordListScore['firstLetVal'] + wordListScore['secondLetVal'] + wordListScore['thirdLetVal'] + wordListScore['fourthLetVal'] + wordListScore['fifthLetVal'])*wordListScore['repeatLets']
+        #print('numofguesses is 0)')
         print(wordListScore.sort_values(by=['score'], ascending=False))
-        wordListScore.loc[:,('active')] = 1
+        #wordListScore.loc[:,('active')] = 1
+        #wordListScore.loc[:,('score')] = (wordListScore['firstLetVal'] + wordListScore['secondLetVal'] + wordListScore['thirdLetVal'] + wordListScore['fourthLetVal'] + wordListScore['fifthLetVal'])*wordListScore['active']
+    elif numOfGuesses == 2:
+        #wordListScore.loc[:,('notGuessed')] = np.where(wordListScore['Words'].apply(lambda x: len(set(x)))!=5, 0, wordListScore['notGuessed'])
         wordListScore.loc[:,('score')] = (wordListScore['firstLetVal'] + wordListScore['secondLetVal'] + wordListScore['thirdLetVal'] + wordListScore['fourthLetVal'] + wordListScore['fifthLetVal'])*wordListScore['active']
-    #elif numOfGuesses == 1:
-    #    wordListScore['notGuessed'] = np.where(wordListScore['Words'].apply(lambda x: len(set(x)))!=5, 0, wordListScore['notGuessed'])
-    #    wordListScore['score'] = (wordListScore['firstLetVal'] + wordListScore['secondLetVal'] + wordListScore['thirdLetVal'] + wordListScore['fourthLetVal'] + wordListScore['fifthLetVal'])*wordListScore['notGuessed']
-    #    print(wordListScore.sort_values(by=['score'], ascending=False))
-    #    wordListScore['score'] = (wordListScore['firstLetVal'] + wordListScore['secondLetVal'] + wordListScore['thirdLetVal'] + wordListScore['fourthLetVal'] + wordListScore['fifthLetVal'])*wordListScore['active']
+        #print('second guess?')
+        print(wordListScore.sort_values(by=['score'], ascending=False))
+        #wordListScore['score'] = (wordListScore['firstLetVal'] + wordListScore['secondLetVal'] + wordListScore['thirdLetVal'] + wordListScore['fourthLetVal'] + wordListScore['fifthLetVal'])*wordListScore['active']
     else:
         wordListScore.loc[:,('score')] = (wordListScore['firstLetVal'] + wordListScore['secondLetVal'] + wordListScore['thirdLetVal'] + wordListScore['fourthLetVal'] + wordListScore['fifthLetVal'])*wordListScore['active']
         print(wordListScore.sort_values(by=['score'], ascending=False))
     return wordListScore
 
+# for manual usage, ingests user's guess
 def getGuess():
     global numOfGuesses
     guess = input("Enter your guess: ")
@@ -107,26 +119,32 @@ def getGuess():
     numOfGuesses += 1
     return guess
 
+# grabs the first letter of the guess
 def getGuessLet1(guess):
     guess1Let1 = guess[0]
     return guess1Let1
 
+# grabs the second letter of the guess
 def getGuessLet2(guess):
     guess1Let2 = guess[1]
     return guess1Let2
 
+# grabs the third letter of the guess
 def getGuessLet3(guess):
     guess1Let3 = guess[2]
     return guess1Let3
 
+# grabs the fourth letter of the guess
 def getGuessLet4(guess):
     guess1Let4 = guess[3]
     return guess1Let4
 
+# grabs the fifth letter of the guess
 def getGuessLet5(guess):
     guess1Let5 = guess[4]
     return guess1Let5
 
+# interprets the user given feedback of the guess for the first letter
 def let1Feedback(guess1Let1,wordList):
     guess1Let1Feed = input("Feedback for letter 1? (g [letter in correct position]/ b [letter is not in word]/ m [letter is in word but not in the correct spot])")
     wordList.loc[:,('notGuessed')] = np.where(wordList['Words'].str.contains(guess1Let1),0,wordList['notGuessed'])
@@ -142,6 +160,7 @@ def let1Feedback(guess1Let1,wordList):
         let1Feedback(guess1Let1, wordList)
     return wordList, guess1Let1Feed
 
+# interprets the user given feedback of the guess for the second letter
 def let2Feedback(guess1Let2, wordList):
     guess1Let2Feed = input("Feedback for letter 2? (g [letter in correct position]/ b [letter is not in word]/ m [letter is in word but not in the correct spot])")
     wordList.loc[:,('notGuessed')] = np.where(wordList['Words'].str.contains(guess1Let2),0,wordList['notGuessed'])
@@ -157,6 +176,7 @@ def let2Feedback(guess1Let2, wordList):
         let2Feedback(guess1Let2, wordList)
     return wordList, guess1Let2Feed
 
+# interprets the user given feedback of the guess for the third letter
 def let3Feedback(guess1Let3, wordList):
     guess1Let3Feed = input("Feedback for letter 3? (g [letter in correct position]/ b [letter is not in word]/ m [letter is in word but not in the correct spot])")
     wordList.loc[:,('notGuessed')] = np.where(wordList['Words'].str.contains(guess1Let3),0,wordList['notGuessed'])
@@ -172,6 +192,7 @@ def let3Feedback(guess1Let3, wordList):
         let3Feedback(guess1Let3, wordList)
     return wordList, guess1Let3Feed
 
+# interprets the user given feedback of the guess for the fourth letter
 def let4Feedback(guess1Let4, wordList):
     guess1Let4Feed = input("Feedback for letter 4? (g [letter in correct position]/ b [letter is not in word]/ m [letter is in word but not in the correct spot])")
     wordList.loc[:,('notGuessed')] = np.where(wordList['Words'].str.contains(guess1Let4),0,wordList['notGuessed'])
@@ -187,6 +208,7 @@ def let4Feedback(guess1Let4, wordList):
         let4Feedback(guess1Let4, wordList)
     return wordList, guess1Let4Feed
 
+# interprets the user given feedback of the guess for the fifth letter
 def let5Feedback(guess1Let5, wordList):
     guess1Let5Feed = input("Feedback for letter 5? (g [letter in correct position]/ b [letter is not in word]/ m [letter is in word but not in the correct spot])")
     wordList.loc[:,('notGuessed')] = np.where(wordList['Words'].str.contains(guess1Let5),0,wordList['notGuessed'])
@@ -202,6 +224,7 @@ def let5Feedback(guess1Let5, wordList):
         let5Feedback(guess1Let5, wordList)
     return wordList, guess1Let5Feed
 
+# asks user if they want to continue the game and guess again
 def guessAgain():
     again = input("Would you like to guess again? (y/n) ")
     if again != 'y' and again != 'n':
@@ -209,43 +232,103 @@ def guessAgain():
         guessAgain()
     return again
 
-def main():
-    empty = False
-    #create df
-    wordleDf = readData()
-    wordleDfTemp = createFullDf(wordleDf)
-    wordleDfTemp = calcScore(wordleDfTemp)
+# for the automated usage, graphs the results of number of guesses it took for the computer to guess each of the words in the input list
+def graphGuesses():
+    global yAxis
+    xAxis = [1,2,3,4,5,6,7,8,9,10]
+    mult = []
+    for i in range(len(xAxis)):
+        mult.append(xAxis[i]*yAxis[i])
+    plt.bar(xAxis,yAxis)
+    plt.title('Avg Guesses = ' + str(sum(mult)/sum(yAxis)))
+    plt.show()
 
-    #get guess and break out the letters
-    guess = getGuess()
-    let1 = getGuessLet1(guess)
-    let2 = getGuessLet2(guess)
-    let3 = getGuessLet3(guess)
-    let4 = getGuessLet4(guess)
-    let5 = getGuessLet5(guess)
-
-    #incorporate game feedback from guess
-    let1FeedDf,let1Feed = let1Feedback(let1, wordleDf)
-    let2FeedDf,let2Feed = let2Feedback(let2, let1FeedDf)
-    let3FeedDf,let3Feed = let3Feedback(let3, let2FeedDf)
-    let4FeedDf,let4Feed = let4Feedback(let4, let3FeedDf)    
-    let5FeedDf,let5Feed = let5Feedback(let5, let4FeedDf)
-
-    updatedDf = createFullDf(let5FeedDf)
-    updatedDf = calcScore(updatedDf)
-
-    if let1Feed== 'g' and let2Feed== 'g' and let3Feed== 'g' and let4Feed== 'g' and let5Feed == 'g' and updatedDf.shape[0] == 1:
-        guessed = True
-    elif updatedDf.shape[0]==0:
-        guessed = True
-        empty = True
+# for the autoamted usage, compares top guess to the desired word
+def compareWords(compGuess, word, wordList):
+    global notGuessed, yAxis, numOfGuesses
+    if compGuess == word:
+        wordList = pd.DataFrame()
+        if numOfGuesses <=10:
+            yAxis[numOfGuesses-1] += 1
+        else:
+            print('too many guesses') 
+        notGuessed = False
     else:
-        guessed = False
+        compData = []
+        for i in range(5):
+            if compGuess[i] in word:
+                compData.append(1)
+            else:
+                compData.append(0)
+
+            if compGuess[i] == word[i]:
+                compData[i] = 2
+
+        for b in range(5):
+            feedback = compData[b]
+            wordList.loc[:,('notGuessed')] = np.where(wordList['Words'].str.contains(compGuess[b]),0,wordList['notGuessed'])
+            if feedback == 2:
+                wordList.loc[:,('active')] = np.where(wordList.iloc[:,b+1]!=compGuess[b],0,wordList['active'])
+            elif feedback == 0:
+                wordList.loc[:,('active')] = np.where(wordList['Words'].str.contains(compGuess[b]),0,wordList['active'])
+            elif feedback == 1:
+                wordList.loc[:,('active')] = np.where(wordList.iloc[:,b+1]==compGuess[b],0,wordList['active'])
+                wordList.loc[:,('active')] = np.where(wordList['Words'].str.contains(compGuess[b]) == False,0,wordList['active']) 
+    return wordList
+
+# for automated usage, selects a word to be guessed
+def generateWord(dfWords):
+    global wordIndex, numOfGuesses, notGuessed
+    notGuessed = True
+    numOfGuesses = 0
+    word = dfWords['Words'][wordIndex]
+    wordIndex += 1
+    #print(str(word), str(numOfGuesses), 'word')
+    return word
+
+# for automated usage, selects the guess that will yield the most information
+def generateGuess(dfScores):
+    global numOfGuesses
+    numOfGuesses += 1
+    guess = pd.DataFrame()
+    guess['word'] = dfScores.sort_values(by=['score'], ascending=False)['Words'].head(1)
+    guess2 = guess.reset_index()
+    #print(str(guess2.loc[0].at['word']),str(numOfGuesses), 'guess')
+    return guess2.loc[0].at['word']
+
+# main code, allows you to pick manual or automated usage of the code to play wordle
+def main():
+    global wordIndex, yAxis
+    #create df
+    empty = False
+    wordIndex = 0
+    yAxis = [0,0,0,0,0,0,0,0,0,0]
+    wordleDf = readData()
 
 
-    while guessed == False:
-        #anotherGuess = guessAgain()
+    runPath = input('Would you like to manually play or automate the game? (m/a) ')
 
+    if runPath == 'a':
+        num = input('How many words would you like to check the results of? (1-12972) ')
+        while wordIndex < int(num):
+            genWord = str(generateWord(wordleDf))
+            wordleDf['active'] = 1
+            wordleDf['notGuessed'] = 1
+            wordleDfTemp = createFullDf(wordleDf)
+            wordleDfTemp = calcScore(wordleDfTemp)
+            
+            genGuess = str(generateGuess(wordleDfTemp))
+            dfComp = compareWords(genGuess, genWord, wordleDf)
+            while notGuessed == True:
+                wordleDfTemp = createFullDf(dfComp)
+                wordleDfTemp = calcScore(wordleDfTemp)
+                genGuess = str(generateGuess(wordleDfTemp))
+                dfComp = compareWords(genGuess, genWord, wordleDf)
+        graphGuesses()   
+    elif runPath == 'm':
+        wordleDf = readData()
+        wordleDfTemp = createFullDf(wordleDf)
+        wordleDfTemp = calcScore(wordleDfTemp)
         #get guess and break out the letters
         guess = getGuess()
         let1 = getGuessLet1(guess)
@@ -273,10 +356,42 @@ def main():
             guessed = False
 
 
-    if guessed == True and empty!=True:
-        print("Congratulations you guessed the word in " + str(numOfGuesses) + " tries")
+        while guessed == False:
+            #anotherGuess = guessAgain()
+
+            #get guess and break out the letters
+            guess = getGuess()
+            let1 = getGuessLet1(guess)
+            let2 = getGuessLet2(guess)
+            let3 = getGuessLet3(guess)
+            let4 = getGuessLet4(guess)
+            let5 = getGuessLet5(guess)
+
+            #incorporate game feedback from guess
+            let1FeedDf,let1Feed = let1Feedback(let1, wordleDf)
+            let2FeedDf,let2Feed = let2Feedback(let2, let1FeedDf)
+            let3FeedDf,let3Feed = let3Feedback(let3, let2FeedDf)
+            let4FeedDf,let4Feed = let4Feedback(let4, let3FeedDf)    
+            let5FeedDf,let5Feed = let5Feedback(let5, let4FeedDf)
+
+            updatedDf = createFullDf(let5FeedDf)
+            updatedDf = calcScore(updatedDf)
+
+            if let1Feed== 'g' and let2Feed== 'g' and let3Feed== 'g' and let4Feed== 'g' and let5Feed == 'g' and updatedDf.shape[0] == 1:
+                guessed = True
+            elif updatedDf.shape[0]==0:
+                guessed = True
+                empty = True
+            else:
+                guessed = False
+
+
+        if guessed == True and empty!=True:
+            print("Congratulations you guessed the word in " + str(numOfGuesses) + " tries")
+        else:
+            print("No words remaining. Thanks for playing")
     else:
-        print("No words remaining. Thanks for playing")
+        print('Not a valid answer')
 
 
 
